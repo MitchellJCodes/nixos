7449997d9679f667453ee2f246fbff5fb9c39f0a
MitchellJCodes <Mitchell.Nazareth2@gmail.com>
2026-07-09 15:10:20 -0400

    Enable xdg.portal with additional portal options

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -35,6 +35,15 @@
 
   services.upower.enable = true;
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+    ];
+  };
+
   nixpkgs.overlays = [
     (final: prev: {
       nautilus = prev.nautilus.overrideAttrs (nprev: {
