b842262c3e10e38273a0ed886ec4a459527b34d2
MitchellJCodes <Mitchell.Nazareth2@gmail.com>
2026-07-13 15:38:27 -0400

    Remove flatpak-repo systemd service configuration
    Removed systemd service configuration for flatpak-repo.

--- modules/services.nix
+++ modules/services.nix
@@ -15,13 +15,6 @@
 
   # Flatpak
   services.flatpak.enable = true;
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
 
   # Appimage support
   programs.appimage = {
