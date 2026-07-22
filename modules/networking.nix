92f205e8e9325f9877d57842a27e12e085569c0d
MitchellJCodes <Mitchell.Nazareth2@gmail.com>
2026-07-21 20:04:36 -0400

    Update networking configuration with DNS settings
    Refactor networkmanager settings and enhance DNS configuration.

--- modules/networking.nix
+++ modules/networking.nix
@@ -4,19 +4,43 @@
   networking = {
     hostName = "nixos-btw";
 
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      dns = "systemd-resolved";
+    };
 
     firewall = {
       enable = true;
 
-      # 53317 Open for Localsend
-      allowedTCPPorts = [
-        53317
+      # LocalSend
+      allowedTCPPorts = [ 53317 ];
+      allowedUDPPorts = [ 53317 ];
+    };
+  };
+
+  services.resolved = {
+    enable = true;
+
+    settings.Resolve = {
+      DNS = [
+        "9.9.9.9"
+        "149.112.112.112"
+        "2620:fe::fe"
+        "2620:fe::9"
       ];
 
-      allowedUDPPorts = [
-        53317
+      FallbackDNS = [
+        "1.1.1.1"
+        "1.0.0.1"
+        "2606:4700:4700::1111"
+        "2606:4700:4700::1001"
       ];
+
+      DNSOverTLS = "true";
+      DNSSEC = "allow-downgrade";
+
+      # Use these resolvers for all domains.
+      Domains = [ "~." ];
     };
   };
 }
