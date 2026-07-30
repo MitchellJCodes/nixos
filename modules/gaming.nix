1b45716de1f588421b3a2dd1cd5889dc52bae5ba
MitchellJCodes <Mitchell.Nazareth2@gmail.com>
2026-07-30 19:03:42 -0400

    Enable Gamemode and add gaming packages
    Added Gamemode support and gaming packages to the configuration.

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,20 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
   };
+
+  # Enable Gamemode
+  programs.gamemode.enable = true;
 
   # Enable Steam
   programs.steam = {
     enable = true;
 
     remotePlay.openFirewall = true;
-
     dedicatedServer.openFirewall = true;
-
     localNetworkGameTransfers.openFirewall = true;
   };
+
+  # Gaming packages
+  environment.systemPackages = with pkgs; [
+    wineWow64Packages.stable
+    winetricks
+    protonplus
+    lutris-free
+    goverlay
+    mangohud
+    gamescope
+  ];
 }
