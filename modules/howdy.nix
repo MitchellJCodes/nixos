221bd7c52d57b4c15b44ff44a1c3cca347588a36
MitchellJCodes <Mitchell.Nazareth2@gmail.com>
2026-08-05 15:39:27 -0400

    Clean up howdy.nix by removing unused settings
    Removed commented-out core settings from howdy configuration.

--- modules/howdy.nix
+++ modules/howdy.nix
@@ -7,12 +7,6 @@
     enable = true;
     control = "sufficient";
 
-    settings = {
-      core = {
-        # timeout = 5;
-        # tolerance = 5;
-      };
-
       video = {
         device_path = "/dev/video2";
       };
