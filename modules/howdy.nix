9add24769a7ed072058145e5d1fefd2240e93f50
MitchellJCodes <Mitchell.Nazareth2@gmail.com>
2026-08-05 13:12:28 -0400

    Add files via upload

---
+++ modules/howdy.nix
@@ -0,0 +1,19 @@
+{ ... }:
+
+{
+  services.howdy = {
+    enable = true;
+    control = "sufficient";
+
+    settings = {
+      core = {
+        # timeout = 5;
+        # tolerance = 5;
+      };
+
+      video = {
+        # device_path = "/dev/video0";
+      };
+    };
+  };
+}
