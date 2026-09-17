e60447e883b3c9f69c03aca1af40bb240ac3d008
Mitchell Nazareth <198786687+MitchellJCodes@users.noreply.github.com>
2026-09-17 17:40:48 -0400

    Add configuration for librewolf

---
+++ modules/librewolf.nix
@@ -0,0 +1,152 @@
+{ pkgs, ... }:
+
+let
+  extension = id: {
+    name = id;
+    value = {
+      installation_mode = "force_installed";
+    };
+  };
+in
+{
+  programs.firefox = {
+    enable = true;
+    package = pkgs.librewolf;
+
+    policies = {
+      # Privacy
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+
+      # Passwords
+      OfferToSaveLogins = false;
+      PasswordManagerEnabled = false;
+
+      # Browser
+      DontCheckDefaultBrowser = true;
+      DisableProfileImport = true;
+
+      # Security
+      # HttpsOnlyMode = "enabled";
+      # EnableTrackingProtection = {
+      #   Value = true;
+      #   Locked = true;
+      #   Cryptomining = true;
+      #   Fingerprinting = true;
+      # };
+
+      # Keep cookies, but isolate third-party cookies
+      # Cookies.Behavior = "partition-foreign";
+
+      # New tab
+      # FirefoxHome = {
+      #   Search = true;
+      #   TopSites = false;
+      #   Highlights = false;
+      #   Pocket = false;
+      #   SponsoredPocket = false;
+      #   SponsoredTopSites = false;
+      #   Snippets = false;
+      # };
+
+      # Mozilla recommendations/promotions
+      # UserMessaging = {
+      #   ExtensionRecommendations = false;
+      #   FeatureRecommendations = false;
+      #   MoreFromMozilla = false;
+      #   FirefoxLabs = false;
+      #   SkipOnboarding = true;
+      # };
+
+      # Suggestions
+      # FirefoxSuggest = {
+      #   WebSuggestions = false;
+      #   SponsoredSuggestions = false;
+      #   ImproveSuggest = false;
+      # };
+
+      # Autofill
+      # AutofillAddressEnabled = false;
+      # AutofillCreditCardEnabled = false;
+
+      # Uncomment if you don't use Firefox Sync.
+      # DisableFirefoxAccounts = true;
+
+      # Extensions
+      ExtensionSettings = builtins.listToAttrs [
+        # uBlock Origin
+        (extension "uBlock0@raymondhill.net")
+
+        # Bitwarden
+        (extension "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+
+        # Dark Reader
+        (extension "addon@darkreader.org")
+
+        # SponsorBlock
+        (extension "sponsorBlocker@ajay.app")
+
+        # Pywalfox
+        (extension "pywalfox@frewacom.org")
+
+        # Hide YouTube Shorts
+        (extension "{88ebde3a-4581-4c6b-8019-2a05a9e3e938}")
+
+        # ...
+      ];
+
+      # Search engines / URL aliases
+      SearchEngines = {
+        Default = "DuckDuckGo";
+
+        Add = [
+          {
+            Name = "nixpkgs packages";
+            URLTemplate = "https://search.nixos.org/packages?query={searchTerms}";
+            IconURL = "https://wiki.nixos.org/favicon.ico";
+            Alias = "@np";
+          }
+
+          {
+            Name = "NixOS options";
+            URLTemplate = "https://search.nixos.org/options?query={searchTerms}";
+            IconURL = "https://wiki.nixos.org/favicon.ico";
+            Alias = "@no";
+          }
+
+          {
+            Name = "NixOS Wiki";
+            URLTemplate = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
+            IconURL = "https://wiki.nixos.org/favicon.ico";
+            Alias = "@nw";
+          }
+
+          {
+            Name = "noogle";
+            URLTemplate = "https://noogle.dev/q?term={searchTerms}";
+            IconURL = "https://noogle.dev/favicon.ico";
+            Alias = "@ng";
+          }
+        ];
+      };
+    };
+
+    # Preferences are outside `policies`.
+    preferences = {
+      "browser.urlbar.suggest.quicksuggest" = false;
+      "browser.urlbar.quicksuggest.enabled" = false;
+      "browser.urlbar.quicksuggest.dataCollection.enabled" = false;
+      "browser.search.suggest.enabled" = false;
+      "browser.discovery.enabled" = false;
+      "browser.newtabpage.activity-stream.feeds.topsites" = false;
+      "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+
+      # Optional: stronger anti-fingerprinting, but can break websites.
+      # "privacy.resistFingerprinting" = true;
+    };
+  };
+
+  environment.etc."firefox/policies/policies.json".target =
+    "librewolf/policies/policies.json";
+}
