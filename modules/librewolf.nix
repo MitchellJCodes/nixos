99226586be2c2f1193cac7b675f1b436dc1803df
Mitchell Nazareth <198786687+MitchellJCodes@users.noreply.github.com>
2026-09-21 13:37:47 -0400

    Removed extra comments/unused opts

--- modules/librewolf.nix
+++ modules/librewolf.nix
@@ -27,52 +27,6 @@ in
       DontCheckDefaultBrowser = true;
       DisableProfileImport = true;
 
-      # Security
-      # HttpsOnlyMode = "enabled";
-      # EnableTrackingProtection = {
-      #   Value = true;
-      #   Locked = true;
-      #   Cryptomining = true;
-      #   Fingerprinting = true;
-      # };
-
-      # Keep cookies, but isolate third-party cookies
-      # Cookies.Behavior = "partition-foreign";
-
-      # New tab
-      # FirefoxHome = {
-      #   Search = true;
-      #   TopSites = false;
-      #   Highlights = false;
-      #   Pocket = false;
-      #   SponsoredPocket = false;
-      #   SponsoredTopSites = false;
-      #   Snippets = false;
-      # };
-
-      # Mozilla recommendations/promotions
-      # UserMessaging = {
-      #   ExtensionRecommendations = false;
-      #   FeatureRecommendations = false;
-      #   MoreFromMozilla = false;
-      #   FirefoxLabs = false;
-      #   SkipOnboarding = true;
-      # };
-
-      # Suggestions
-      # FirefoxSuggest = {
-      #   WebSuggestions = false;
-      #   SponsoredSuggestions = false;
-      #   ImproveSuggest = false;
-      # };
-
-      # Autofill
-      # AutofillAddressEnabled = false;
-      # AutofillCreditCardEnabled = false;
-
-      # Uncomment if you don't use Firefox Sync.
-      # DisableFirefoxAccounts = true;
-
       # Extensions
       ExtensionSettings = builtins.listToAttrs [
         # uBlock Origin
@@ -141,9 +95,6 @@ in
       "browser.discovery.enabled" = false;
       "browser.newtabpage.activity-stream.feeds.topsites" = false;
       "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-
-      # Optional: stronger anti-fingerprinting, but can break websites.
-      # "privacy.resistFingerprinting" = true;
     };
   };
 
